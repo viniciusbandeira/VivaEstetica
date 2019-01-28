36de6e61df69fbd9ba4372fc11cef62346c9c514
Vinicius Bandeira <bandeiravinicius0@live.com>
2019-01-28 19:40:02 -0200

    Criando login e logout

--- VivaEstitica/Controllers/HomeController.cs
+++ VivaEstitica/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VivaEstitica.ViewModels;
+using VivaEstitica.Models;
 
 namespace VivaEstitica.Controllers
 {
     public class HomeController : Controller
     {
+        private VivaEsteticaContext db = new VivaEsteticaContext();
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +29,32 @@ namespace VivaEstitica.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Login()
+        {
+            var dadosLogin = new DadosLogin();
+            return View(dadosLogin);
+        }
+
+        [HttpPost]
+        public ActionResult Login(DadosLogin dadosLogin)
+        {
+            if (dadosLogin.Autentica(db))
+            {
+                Session["user"] = dadosLogin;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+
+        }
+        public ActionResult Logout()
+        {
+            Session["user"] = null;
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ VivaEstitica/ViewModels/DadosLogin.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using VivaEstitica.Models;
+
+namespace VivaEstitica.ViewModels
+{
+    public enum TipoUsuario { gerenciador, cliente, profissional};
+    public class DadosLogin
+    {
+        [Key]
+        public string documento { get; set; }
+        [Required]
+        public string senha { get; set; }
+        public TipoUsuario? tipoUsuario;
+
+
+        public bool Autentica(VivaEsteticaContext context)
+        {
+            List<AbstractUsuario> listaUsuarios = new List<AbstractUsuario>();
+            listaUsuarios.AddRange(context.Gerenciadors);
+            listaUsuarios.AddRange(context.Profissionais);
+            listaUsuarios.AddRange(context.Clientes);
+            AbstractUsuario usuario = null;
+            foreach (var u in listaUsuarios)
+            {
+                if (u.Documento.Equals(this.documento))
+                {
+                    usuario = u;
+                    break;
+                }
+
+            }
+
+            
+
+            if(usuario != null && usuario.ValidaSenha(senha))
+            {
+                if (usuario.GetType() is Gerenciador)
+                {
+                    this.tipoUsuario = TipoUsuario.gerenciador;
+                }
+                else if (usuario.GetType() is Profissional)
+                {
+                    this.tipoUsuario = TipoUsuario.profissional;
+                }
+                else if (usuario.GetType() is Cliente)
+                {
+                    this.tipoUsuario = TipoUsuario.cliente;
+                }
+
+                    return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
